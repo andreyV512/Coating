d922f78d60eb15cf18cb68b4e6ee522cde277c0e
Uran <nobady@mail.net>
2019-11-25 09:37:06 +0300

    xx

---
+++ Plast/Windows/GridWindow/GridWindow.cpp
@@ -0,0 +1,74 @@
+#include "GridWindow.h"
+#include "Grid/GridDetail.h"
+#include "window_tool/MenuAPI.h"
+
+DataGrid::DataGrid() : okButton(*this) {}
+void DataGrid::Size(int x, int y, int width, int heigth) 
+{ 
+	MoveWindow(hWnd, x, y, width, heigth, TRUE); 
+}
+LRESULT DataGrid::Do(TNotify &l)
+{
+	return  GridDetail::Do(this, l);
+}
+
+DataGrid::OkButton::OkButton(DataGrid &o) : owner(o) {}
+
+
+LRESULT GridWindowX::operator()(TCreate &l)
+{
+	hWnd = l.hwnd;
+	hBtnOk = CreateWindow(L"button", L"Применить"
+		, WS_VISIBLE | WS_CHILD | WS_TABSTOP
+		, 0, 0, 0, 0
+		, l.hwnd
+		, NULL
+		, GetModuleHandle(NULL)
+		, NULL
+	);
+	SetWindowLongPtr(hBtnOk, GWLP_USERDATA, NULL);
+	hBtnCancel = CreateWindow(L"button", L"Отмена"
+		, WS_VISIBLE | WS_CHILD | WS_TABSTOP
+		, 0, 0, 0, 0
+		, l.hwnd
+		, NULL
+		, GetModuleHandle(NULL)
+		, NULL
+	);
+	SetWindowLongPtr(hBtnCancel, GWLP_USERDATA, (LONG_PTR)&cancelButton);
+	return 0;
+}
+
+void GridWindowX::operator()(TDestroy &l)
+{
+	SetWindowLongPtr(l.hwnd, GWLP_USERDATA, NULL);
+	delete this;
+}
+
+void GridWindowX::operator()(TSize &l)
+{
+	RECT r;
+	GetClientRect(l.hwnd, &r);
+	grid->Size(0, 0, r.right, r.bottom - 50);
+	const int top = r.bottom - 40;
+	const int height = 30;
+	const int width = r.right / 2 - 40;
+
+	MoveWindow(hBtnOk, 20, top, width, height, TRUE);
+	MoveWindow(hBtnCancel, 60 + width, top, width, height, TRUE);
+}
+
+LRESULT GridWindowX::operator()(TNotify &l)
+{
+	return EventDo(l);
+}
+
+void GridWindowX::operator()(TCommand &l)
+{
+	EventDo(l);
+}
+
+void GridWindowX::CancelButton::Do(TCommand &l)
+{
+	DestroyWindow(l.hwnd);
+}
---
+++ Plast/Windows/GridWindow/GridWindow.h
@@ -0,0 +1,85 @@
+#pragma once
+#include "window_tool/Emptywindow.h"
+#include "window_tool/TEvent.h"
+#include <CommCtrl.h>
+
+class DataGrid : public TEvent
+{
+public:
+	struct OkButton : TEvent
+	{
+		DataGrid &owner;
+		OkButton(DataGrid &);
+		void Do(TCommand &) {}
+	} okButton;
+	HWND hWnd;
+	DataGrid();
+	virtual void Create(HWND) = 0;
+	void Size(int, int, int, int);
+	LRESULT Do(TNotify &);
+	virtual void RClick(LPNMITEMACTIVATE) = 0;
+	virtual void SetDataToGrid(LV_DISPINFO *) = 0;
+	virtual void SetColorToGrid(NMLVCUSTOMDRAW *) = 0;
+};
+
+class GridWindowX
+{
+public:
+	HWND hBtnOk, hBtnCancel;
+public:
+	struct CancelButton : TEvent
+	{
+		void Do(TCommand &);
+	} cancelButton;
+	HWND hWnd;
+	DataGrid *grid;
+	LRESULT operator()(TCreate &);
+	void operator()(TDestroy &);
+	void operator()(TSize &);
+	LRESULT operator()(TNotify &);
+	void operator()(TCommand &);
+};
+
+template<class T>class GridOptions : public GridWindowX
+{
+	T _grid;
+public:
+	typedef GridWindowX Parent;
+	LRESULT operator()(TCreate &l)
+	{
+		grid = &_grid;
+		grid->Create(l.hwnd);
+		LRESULT res = Parent::operator()(l);
+		SetWindowLongPtr(hBtnOk, GWLP_USERDATA, (LONG_PTR)&_grid.okButton);
+		return res;
+	}
+	static void Open(wchar_t *title, int width, int height)
+	{
+		typedef GridOptions<T> Owner;
+		WindowClass<Owner> x;
+		HWND hh = FindWindow(x(), 0);
+		if (NULL != hh)
+		{
+			SendMessage(hh, WM_SYSCOMMAND, SC_RESTORE, 0);
+			SetForegroundWindow(hh);
+		}
+		else
+		{
+			RECT r;
+			GetClientRect(GetDesktopWindow(), &r);
+			hh = WindowTemplate(new Owner, title
+				, (r.right - width) / 2
+				, (r.bottom - height) / 2
+				, width
+				, height
+				, IDI_settings
+				, (HBRUSH)COLOR_WINDOW
+			);
+			LONG_PTR s = GetWindowLongPtr(hh, GWL_STYLE);
+			s &= ~(WS_MAXIMIZEBOX | WS_MINIMIZEBOX | WS_THICKFRAME);
+			SetWindowLongPtr(hh, GWL_STYLE, s);
+			SetWindowPos(hh, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+			ShowWindow(hh, SW_SHOWNORMAL);
+		}
+	}
+};
