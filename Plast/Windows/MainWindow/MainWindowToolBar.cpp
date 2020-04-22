003ded43bd65a5d07d35337ed5701696ddef9848
uran <nobody@mail.net>
2020-04-22 11:26:10 +0500

    main window tool bar

--- Plast/Windows/MainWindow/MainWindowToolBar.cpp
+++ Plast/Windows/MainWindow/MainWindowToolBar.cpp
@@ -5,6 +5,7 @@
 #include "window_tool/OpenWindow.hpp"
 #include "Windows/AScanWindow/AScanWindow.h"
 #include "Windows/MainWindow/AppKeyHandler.h"
+#include "Automat/Automat.h"
 
 namespace
 {
@@ -17,7 +18,7 @@ namespace
 };
 
 	KEY(IDB_CycleBtn, L"F4 Цикл")
-		KEY(IDB_Reset, L"Esc Стор")
+	KEY(IDB_Reset, L"Esc Стор")
 //		KEY(IDB_QueryBtn, L"Тест")
 #undef KEY
 
@@ -32,15 +33,17 @@ namespace
 
 	void Key<IDB_CycleBtn>::Click(HWND h) 
 	{
-		dprint("Key<IDB_CycleBtn>::Click\n"); 
-		AppKeyHandler::Run();
-		App::IsRun() = true;
+		//dprint("Key<IDB_CycleBtn>::Click\n"); 
+		//AppKeyHandler::Run();
+		//App::IsRun() = true;
+		Automat::Start();
 	}
 	void Key<IDB_Reset>::Click(HWND h) 
 	{
-		dprint("Key<IDB_Reset>::Click\n"); 
-		AppKeyHandler::Stop();
-		App::IsRun() = false;
+		//dprint("Key<IDB_Reset>::Click\n"); 
+		//AppKeyHandler::Stop();
+		//App::IsRun() = false;
+		Automat::Stop();
 	}
 	//void Key<IDB_QueryBtn>::Click(HWND h) 
 	//{
