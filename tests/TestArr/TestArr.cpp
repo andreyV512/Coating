b388506ef5b9b92d4e625af0879f0241428e2f44
uran <nobody@mail.net>
2020-04-22 11:25:28 +0500

    test arr

---
+++ tests/TestArr/TestArr.cpp
@@ -0,0 +1,27 @@
+﻿// TestArr.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "templates\typelist.hpp"
+
+template<class O, class P>struct tst
+{
+    bool operator()()
+    {
+        printf("%s\n", typeid(O).name());
+        return true;
+    }
+};
+
+struct A {};
+struct B {};
+struct C {};
+struct D {};
+
+typedef Vlst<A, B, C, D> list;
+VL::Factory<list> f;
+
+int main()
+{
+    VL::find<list, tst>()();
+}
