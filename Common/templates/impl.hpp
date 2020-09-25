77f6df58e879f5d2f4092bd7fa64a5dbc99e34f9
Andrey <nobody@mail.net>
2020-09-25 11:27:07 +0500

    impl

--- Common/templates/impl.hpp
+++ Common/templates/impl.hpp
@@ -1,16 +1,18 @@
 #pragma once
-template<class T, int size>struct impl
+template<class T, int size>struct Impl
 {
 	template<int size, bool b> struct the_buffer_size_must_be_larger_than_the_type_size;
 	template<int size> struct the_buffer_size_must_be_larger_than_the_type_size<size, true> {};
-	union {
-		T *o;
-		char buf[size];
-	}inner;
-	template<class Z, class ...Params>Z &Set(Params... p)
+	char buf[size];
+	Impl() { *(unsigned *)buf = 0; }
+	template<class Z, class ...Params>Z &Init(Params... p)
 	{
 		the_buffer_size_must_be_larger_than_the_type_size<sizeof(Z), sizeof(Z) <= size>();
-		return *new(inner.buf)Z(p...);
+		if (0 != *(unsigned *)buf)((T *)buf)->~T();
+		return *new(buf)Z(p...);
+	}
+	~Impl() { 
+		if (0 != *(unsigned *)buf)((T *)buf)->~T();
 	}
 };
 
@@ -40,8 +42,14 @@ struct C : B
 
 int main()
 {
-	int k = 1234;
-	A &a = impl<A, 272>().Set<C>((char *)"Ok obi", k);
-	a.print();
+	int k = 0;
+	{
+		Impl<A, 272> impl;
+		A &a = impl.Init<C>((char *)"Ok obi", k);
+		a.print();
+		impl.Init<B>((char *)"HI Ok obi");
+		a.print();
+		printf("   %d\n", (int)sizeof(B));
+	}
 }
 */
