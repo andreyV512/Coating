1f69d6561ca3272e60d2a414f9a7bd191a263222
Andrey <nobody@mail.net>
2020-10-12 10:41:30 +0500

    test namepipe

---
+++ tests/Pipe/client/client.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <conio.h>
+
+const wchar_t *ready_data = L"ready_data";
+
+unsigned k = 99;
+char buf[4058];
+
+int main(int argc, char **argv)
+{
+    HANDLE hEvent = OpenEvent(EVENT_ALL_ACCESS, FALSE, ready_data);
+    HANDLE hWritePipe = (HANDLE)atoi(argv[1]);
+
+    DWORD bytesWritten;
+    while (true)
+    {
+        *(unsigned *)buf = k;
+
+        if (!WriteFile(hWritePipe, buf, 4058, &bytesWritten, NULL))
+        {
+            DWORD ret = GetLastError();
+            _cprintf("client WriteFalted %d\n", ret);
+            _getch();
+            return ret;
+        }
+     //   SetEvent(hEvent);
+        _cprintf("client number %d bytes written %d\n", k, bytesWritten);
+    //    Sleep(100);
+        ++k;
+    }
+    CloseHandle(hWritePipe);
+    _cputs("Press any key\n");
+    _getch();
+}
+
---
+++ tests/Pipe/server/server.cpp
@@ -0,0 +1,93 @@
+﻿#include <windows.h>
+#include <conio.h>
+
+const wchar_t *ready_data = L"ready_data";
+
+int main()
+{
+
+    HANDLE hEvent = CreateEvent(NULL, FALSE, FALSE, ready_data);
+    HANDLE hReadPipe, hWritePipe, hInheritWritePipe;
+
+    if (!CreatePipe(&hReadPipe, &hWritePipe, NULL, 0))
+    {
+        _cputs("Create pipe failed\n");
+        _getch();
+        return GetLastError();
+    }
+
+    if (!DuplicateHandle(
+        GetCurrentProcess()
+        , hWritePipe
+        , GetCurrentProcess()
+        , &hInheritWritePipe
+        , 0
+        , TRUE
+        , DUPLICATE_SAME_ACCESS
+    ))
+    {
+        _cputs("Duplicate handle  failed\n");
+        _getch();
+        return GetLastError();
+    }
+    CloseHandle(hWritePipe);
+
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory(&si, sizeof(si));
+
+    si.cb = sizeof(si);
+    wchar_t buf[180];
+    wsprintf(buf, L"H:\\projects\\Test\\Coating\\Debug\\client.exe %d", (int)hInheritWritePipe);
+
+    if (!CreateProcess(
+        NULL
+        , buf
+        , NULL
+        , NULL
+        , TRUE
+        , CREATE_NEW_CONSOLE
+        , NULL
+        , NULL
+        , &si
+        , &pi
+    ))
+    {
+       
+        DWORD ret = GetLastError();
+        _cprintf("Create process failed  %x\n", ret);
+        _getch();
+        return ret;
+    }
+
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    CloseHandle(hInheritWritePipe);
+    char buffer[4058];
+    DWORD bytesReaded;
+    while (true)
+    {
+      //  WaitForSingleObject(hEvent, INFINITE);
+        if (!ReadFile(
+            hReadPipe
+            , buffer
+            , 4058
+            , &bytesReaded
+            , NULL
+        ))
+        {
+            _cputs("Read from the pipe failed\n");
+            _getch();
+            return GetLastError();
+        }
+        _cprintf("number %d bytes readed %d\n", *(int *)buffer, bytesReaded);
+    }
+    
+    CloseHandle(hReadPipe);
+    _cputs("Process finished\n");
+    _getch();
+    return 0;
+}
+
