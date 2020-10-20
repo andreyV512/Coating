21f5b92e33656239f2a1905c7b3c7631b3410c54
Andrey <nobody@mail.net>
2020-10-20 09:03:28 +0500

    test zip

---
+++ tests/testZip/testZip.cpp
@@ -0,0 +1,16 @@
+﻿// testZip.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "window_tool/Zip.h"
+//
+#pragma comment(lib, "Shlwapi.lib")
+
+int main()
+{
+    //Zip::ZipAsyncAll();
+    Zip::Unzip((wchar_t *)L"z.dat.bz2");
+    std::cout << "Hello World!\n";
+    getchar();
+}
+
