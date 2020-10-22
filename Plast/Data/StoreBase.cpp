ee0a055c26462781aea21ca8bcf2dc10d14507a6
Andrey <nobody@mail.net>
2020-10-22 15:51:21 +0500

    store base

---
+++ Plast/Data/StoreBase.cpp
@@ -0,0 +1,66 @@
+#include "StoreBase.h"
+#include "Base/ExpressBase.hpp"
+#include "Base/tables.hpp"
+#include "Data/Data.h"
+#include "tools_debug/DebugMess.h"
+
+template<class T>int GetID(CBase &base)
+{
+	auto &t = Singleton<T>::Instance();
+	int id = Select<T>(base).Execute(t);
+	if (0 == id)
+	{
+		Insert_Into<T>(t, base).Execute();
+		id = Select<T>(base).Execute(t);
+	}
+	return id;
+}
+
+void Store::Result()
+{
+	StoreBase param;
+	CreateDataBase<StoreBase::type_list, Vlst<>, MSsql> createDateBase;
+	CExpressBase base(
+		(wchar_t *)param.name()
+		, createDateBase
+		, param.tables
+	);
+	if (base.IsOpen())
+	{
+		auto &dt = Singleton<DataTable>::Instance();
+		auto &rd = Singleton<Data::ResultData>::Instance();
+
+		double *d = dt.items.get<ZonesData>().value.buffer;
+		int &count = dt.items.get<ZonesData>().value.count;
+
+		memmove(d, rd.minData, rd.count * sizeof(double));
+		d += rd.count;
+		memmove(d, rd.maxData, rd.count * sizeof(double));
+		count = 2 * rd.count;
+
+		char *st = dt.items.get<ZonesStatus>().value.buffer;
+		memmove(st, rd.status, rd.count * sizeof(char));
+		dt.items.get<ZonesStatus>().value.count = rd.count;
+
+		dt.items.get<ID<TresholdsTable>>().value = GetID<TresholdsTable>(base);
+		dt.items.get<ID<DeadZonesTable>>().value = GetID<DeadZonesTable>(base);
+		dt.items.get<ID<UserTable>>().value = GetID<UserTable>(base);
+
+		Insert_Into< DataTable>(dt, base).Execute();
+	}
+}
+
+void Store::Init()
+{
+	StoreBase param;
+	CreateDataBase<StoreBase::type_list, Vlst<>, MSsql> createDateBase;
+	CExpressBase base(
+		(wchar_t *)param.name()
+		, createDateBase
+		, param.tables
+	);
+	if (base.IsOpen())
+	{
+		dprint("Store base ok");
+	}
+}
---
+++ Plast/Data/StoreBase.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "App/AppBase.h"
+#include "Base/TablesDefine.h"
+#include "App/App.h"
+
+VAR_ARR(ZonesData, double, 2 * App::count_zones)
+VAR_ARR(ZonesStatus, char, App::count_zones)
+
+DEFINE_PARAM_ID(UserTable, int, __id__)
+
+struct DataTable
+{
+	typedef Vlst<
+		ID<TresholdsTable>
+		, ID<DeadZonesTable>
+		, ID<UserTable>
+		, ZonesData
+		, ZonesStatus
+	> items_list;
+	typedef VL::Factory<items_list> TItems;
+	TItems items;
+	const wchar_t *name() { return L"DataTable"; }
+};
+
+struct StoreBase
+{
+	typedef Vlst<
+		TresholdsTable
+		, DeadZonesTable
+		, UserTable
+		, DataTable
+	> type_list;
+
+	typedef VL::Factory<type_list> TTables;
+	TTables tables;
+	const wchar_t *name() { return L"StoreBase"; }
+};
+
+namespace Store
+{
+	void Result();
+	void Init();
+}
+
+
