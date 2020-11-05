c2606a763034aa73642c16ef6dfc7ecfff270326
Andrey <nobody@mail.net>
2020-11-05 12:57:08 +0500

    testStore

---
+++ tests/TestStore/TestStore/Base/Base.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.OleDb;
+
+namespace BaseX.Base
+{
+    public class Base
+    {
+        public OleDbConnection conn;
+
+        public bool Open()
+        {
+            try
+            {
+                conn = new OleDbConnection("File Name=.\\base.udl");
+                conn.Open();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool IsOpen()
+        {
+            return null != conn && System.Data.ConnectionState.Open == conn.State;
+        }
+        public void Close()
+        {
+            if (IsOpen())
+            {
+                conn.Close();
+            }
+        }
+    }
+}
---
+++ tests/TestStore/TestStore/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Data.OleDb;
+
+
+namespace TestStore
+{
+    class Program
+    {
+        public class UserTable
+        {
+            public string UserName;// { get; set; }
+            public int UserPersonnelNumber;// { get; set; }
+        }
+        static void TestUserTable()
+        {
+            var b = new BaseX.Base.Base();
+            if (b.Open())
+            {
+                Console.WriteLine("Base open");
+                string[] m = { "Ifanoff", "Petroff", "Sidoroff", "Xrenoff" };
+                int k = 5;
+                try
+                {
+                    foreach (var i in m)
+                    {
+                        OleDbCommand cmd = b.conn.CreateCommand();
+                        cmd.CommandText = "INSERT INTO UserTable(UserName, UserPersonnelNumber)VALUES(?, ?)";
+                        cmd.Parameters.Add(new OleDbParameter("", OleDbType.VarChar) { Value = i });
+                        cmd.Parameters.Add(new OleDbParameter("", OleDbType.Integer) { Value = k++ });
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (System.Data.OleDb.OleDbException)
+                {
+                    Console.WriteLine("Err");
+                }
+            }
+        }
+
+        static void Conv<T>(ref T t, Object o) => t = (T)o;
+
+        static void TestGetUser()
+        {
+            var u = new UserTable();
+            var b = new BaseX.Base.Base();
+            if (b.Open())
+            {
+                OleDbCommand cmd = b.conn.CreateCommand();
+                cmd.CommandText = "SELECT UserName, UserPersonnelNumber FROM UserTable";
+                OleDbDataReader reader = cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    Conv(ref u.UserName, reader[0]);
+                    Conv(ref u.UserPersonnelNumber, reader[1]);
+                    Console.WriteLine("{0}  {1}", u.UserName, u.UserPersonnelNumber);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //TestUserTable();
+            // UserTable ut;
+            TestGetUser();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
