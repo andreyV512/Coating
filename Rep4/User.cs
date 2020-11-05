f68eff775beb5c8cf8d91dc8d4942db1c9fce1d0
Andrey <nobody@mail.net>
2020-11-05 13:00:29 +0500

    report

---
+++ Rep4/User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Reporting.WinForms;
+
+namespace Rep4
+{
+    public class UserTable
+    {
+        public string UserName { get; set; }
+        public int UserPersonnelNumber { get; set; }
+    }
+
+    class User
+    {
+        static public void Query(ReportViewer viewer)
+        {
+            var l = new List<UserTable>();
+            var param = new List<ReportParameter>();
+            var b = new Base();
+            if (b.Open())
+            {
+                {
+                    OleDbCommand cmd = b.conn.CreateCommand();
+                    cmd.CommandText = "SELECT UserName, UserPersonnelNumber FROM UserTable";
+                    OleDbDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var u = new UserTable();
+                        u.UserName = (string)reader[0];
+                        u.UserPersonnelNumber = (int)reader[1];
+                        l.Add(u);
+                    }
+                }
+                {
+                    OleDbCommand cmd = b.conn.CreateCommand();
+                    cmd.CommandText = "SELECT COUNT(*) FROM UserTable";
+                    var count = cmd.ExecuteScalar().ToString();
+                    param.Add(new ReportParameter("countUsers", count));
+                    
+                }
+                viewer.LocalReport.DataSources.Clear();
+                viewer.LocalReport.ReportPath = @".\ReportUser.rdlc";
+                var dataset = new ReportDataSource("dataSetUser", l);
+                viewer.LocalReport.DataSources.Add(dataset);
+                viewer.LocalReport.SetParameters(param);
+                viewer.LocalReport.Refresh();
+                viewer.RefreshReport();
+                b.Close();
+            }
+        }
+    }
+}
