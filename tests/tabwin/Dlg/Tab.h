7d4c28ea218637f8a366f6745c1fe873a806fe1c
Andrey <nobody@mail.net>
2021-01-15 12:41:09 +0500

    test tab

---
+++ tests/tabwin/Dlg/Tab.h
@@ -0,0 +1,6 @@
+#pragma once 
+#include <Windows.h>
+struct Tab
+{
+	static void Do(HWND);
+};
---
+++ tests/tabwin/tabwin.cpp
@@ -0,0 +1,32 @@
+﻿// tabwin.cpp : Определяет точку входа для приложения.
+//
+
+#include "framework.h"
+#include "tabwin.h"
+#include "Dlg/Tab.h"
+#include <CommCtrl.h>
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    INITCOMMONCONTROLSEX icex;
+    icex.dwSize = sizeof(INITCOMMONCONTROLSEX);
+    icex.dwICC = ICC_TAB_CLASSES;
+    InitCommonControlsEx(&icex);
+
+    Tab::Do(NULL);
+
+    MSG msg;
+
+    // Цикл основного сообщения:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+    }
+
+    return (int) msg.wParam;
+}
+#pragma comment(lib, "comctl32.lib")
