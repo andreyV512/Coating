565656d4b04a3340c617602c68902ce45f178f35
Andrey <nobody@mail.net>
2021-04-06 16:26:48 +0500

    udl

---
+++ tests/ParseUDL/ParseUDL.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+#include <filesystem>
+
+
+
+int main()
+{
+    const wchar_t* path = L".\\connection.udl";
+    const wchar_t* s = L"Provider = SQLOLEDB.1; Integrated Security = SSPI; Persist Security Info = False; Initial Catalog = StoreBase; Data Source = (local)";
+    if (!std::filesystem::exists(path))
+    {
+        std::wofstream fs(path);
+        fs << L"[oledb]" << std::endl;
+        fs << L"; Everything after this line is an OLE DB initstring" << std::endl;
+        fs << s << std::endl;
+        fs.close();
+    }
+    else
+    {
+        std::cout << "err\n";
+    }
+}
+
